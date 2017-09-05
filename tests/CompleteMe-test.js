96f8709c1fda988bdfb48f69cde7b685c7e8f8f0
Ben Porter <bbp5280@gmail.com>
2017-09-05 15:55:02 -0600

    phase 1 set

--- tests/CompleteMe-test.js
+++ tests/CompleteMe-test.js
@@ -1,2 +1,28 @@
 const { assert, expect } = require ('chai');
 const CompleteMe = require ('../scripts/completeMe.js');
+
+let completion;
+
+describe('CompleteMe', () => {
+
+  beforeEach ( ()=> {
+    completion = new CompleteMe
+  })
+
+  it('should add words to the dictionary', () => {
+
+    completion.insert('pizza')
+    assert.equal(completion.dictionary[0], 'pizza');
+  });
+
+  it('number of words should increase by 1 when a word is inserted', () => {
+
+    completion.insert('pizza')
+    assert.equal(completion.numOfWords, 1);
+
+    completion.insert('apple')
+    assert.equal(completion.numOfWords, 2);
+  });
+
+
+})
